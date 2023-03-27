13a5b2721a2c09071272b1c5fad6f8a4c76086d3
dev-leandroborges <devleandroborges>
2023-03-27 16:04:16 -0300

    Estrutura HTML da calculadora feita no src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 function App () {  
   return (
     <div className="App">
-     
+        <h1>Calculadora</h1>
+        <label>Primeiro Nº</label>
+        <input type="number" />
+        <label>Segundo Nº</label>
+        <input type="number" />
+
+        <select>
+          <option>Somar</option>
+          <option>Subtrair</option>
+          <option>Multiplicar</option>
+          <option>Dividir</option>
+        </select>
+
+        <label>Resltado 0</label>
     </div>
   )
 }
